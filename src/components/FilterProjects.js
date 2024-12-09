57e667b3d36ba63ec84093f57efa4acf62eeb54b
Siofra <n00220387@iadt.ie>
2024-12-09 16:55:38 +0000

    filter projects + categories

---
+++ src/components/FilterProjects.js
@@ -0,0 +1,32 @@
+import categoriesList from '../data/categories.json;'
+
+const FilterProjects = ({ setSearchTerm, setSelectedCategory }) => {
+
+    let categoryOptions = categoriesList.map((category, index) => {
+
+        return <option key={index} value={category.value}>{category.label}</option>
+    })
+
+    const handleSearchChange = (e) => {
+        setSearchTerm(e.target.value);
+    };
+    <label labelclassName="input input-bordered flex items-center gap-2">
+        {/* <input onChange(e) => setSearchTerm(e.target.value)} type="text" className="grow" placeholder=""/> */}
+    </label>
+    return (
+        <div>
+            <input
+                type="text"
+                id="search"
+                placeholder="Search by title"
+                onChange={handleSearchChange}
+            />
+            <select onChange={(e) => setSelectedCategory(e.target.value)} name="category">
+                {categoryOptions}
+            </select>
+        </div>
+
+    );
+};
+
+export default FilterProjects;
--- src/pages/projects/Index.js
+++ src/pages/projects/Index.js
@@ -1,14 +1,54 @@
 import { useState, useEffect } from "react";
 import projectsJSON from '../../components/data/projects.json';
 import ProjectCard from "../../components/ProjectCard";
-import { Grid, GridItem } from "@chakra-ui/react"
+import { Grid } from "@chakra-ui/react"
+import FilterProjects from "../../components/FilterProjects";
 // import { Button } from "../../components/ui/button"
 
 
 const Index = () => {
     const [projectsList, setProjectsList] = useState(projectsJSON);
+    const [searchTerm, setSearchTerm] = useState("");
+    const [filteredList, setFilteredList] = useState(projectsJSON);
+    const [selectedCategory, setSelectedCategory] = useState("All")
 
-    const projectsCards = projectsList.map((project, index) => {
+    useEffect(() => {
+        console.log(searchTerm)
+
+        if (searchTerm.length === "") {
+            setFilteredList(projectsList)
+        } else if (searchTerm.length <= 1) {
+            return;
+        }
+        else {
+            let result = projectsList.filter((project) => {
+                return project.title.toLowerCase().includes(searchTerm.toLowerCase());
+
+
+            });
+
+            setFilteredList(result)
+        }
+    }, [searchTerm])
+
+    useEffect(() => {
+        console.log(searchTerm)
+
+        if (selectedCategory === "All") {
+            setFilteredList(projectsList)
+        } 
+        else {
+            let result = projectsList.filter((project) => {
+                return project.categories.includes(selectedCategory);
+
+
+            });
+
+            setFilteredList(result)
+        }
+    }, [selectedCategory])
+
+    const projectsCards = filteredList.map((project, index) => {
         return <ProjectCard key={index} project={project} />
 
     });
@@ -16,6 +56,7 @@ const Index = () => {
     return (
         <>
             <h1><b>Projects</b></h1>
+            <FilterProjects setSearchTerm={setSearchTerm} setSelectedCategory={setSelectedCategory} />
             <Grid templateColumns="repeat(2, 1fr)" gap="6">
                 {projectsCards}
             </Grid>
